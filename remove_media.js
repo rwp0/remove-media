ef5e93d2cda2b604886c42b2c86dc721d4276060
Elvin Aslanov <rwp.primary@gmail.com>
2025-01-28 18:46:47 +0100

    Update remove_media.js

--- remove_media.js
+++ remove_media.js
@@ -1,20 +1,33 @@
 // ==UserScript==
-// @namespace    Focus
 // @name         Remove media
+// @namespace    Personal
 // @description  Remove images and videos
-// @version      2025-01-17
+// @version      2025-01-27
 // @author       Elvin Aslanov
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikimedia.org
-// @license      MIT
 // @grant        none
+// @downloadURL  https://update.greasyfork.org/scripts/524069/Remove%20media.user.js
+// @updateURL    https://update.greasyfork.org/scripts/524069/Remove%20media.meta.js
 // @match        *://*/*
+// @exclude      *.google.com
 // ==/UserScript==
- 
+
+/* eslint no-return-assign: off */
+
 (function() {
   'use strict';
- 
-  for (var element of document.querySelectorAll('img, video, figure, svg, embed')) { // NodeList
-    element.remove();
-  }
- 
+
+document
+  .querySelectorAll('img, video, figure, svg') // NodeList
+    .forEach(element => element.remove());
+
+document
+  .querySelectorAll('*')
+    .forEach( element =>
+      element.style.backgroundImage = null
+    ); // CSSStyleDeclaration
+
+
 })();
+
+// TODO: https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet
