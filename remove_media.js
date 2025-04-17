3c51294084a64b1f6caef0bbe6f17a2d98f0de08
Elvin Aslanov <rwp.primary@gmail.com>
2025-04-17 15:36:19 +0200

    Update code
    Use @run-at document-idle

--- remove_media.js
+++ remove_media.js
@@ -1,33 +1,25 @@
 // ==UserScript==
 // @name         Remove media
-// @namespace    Personal
+// @namespace    Filters
 // @description  Remove images and videos
-// @version      2025-01-27
+// @version      2025-04-17
 // @author       Elvin Aslanov
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikimedia.org
 // @grant        none
 // @downloadURL  https://update.greasyfork.org/scripts/524069/Remove%20media.user.js
 // @updateURL    https://update.greasyfork.org/scripts/524069/Remove%20media.meta.js
-// @match        *://*/*
-// @exclude      *.google.com
+// @run-at document-idle
+// @match *://*/*
 // ==/UserScript==
+
 
 /* eslint no-return-assign: off */
 
-(function() {
-  'use strict';
 
 document
-  .querySelectorAll('img, video, figure, svg') // NodeList
-    .forEach(element => element.remove());
+  . querySelectorAll ( 'img, video, figure, svg' )
+  . forEach ( element => element . remove () );
 
 document
-  .querySelectorAll('*')
-    .forEach( element =>
-      element.style.backgroundImage = null
-    ); // CSSStyleDeclaration
-
-
-})();
-
-// TODO: https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet
+  . querySelectorAll ('*')
+  . forEach ( element => element.style.setProperty ( 'background-image', 'none', 'important' ) );
